a1342533e17b943f06c2668be1f4a506a892cd01
Jhon Habi <jhoninsua-03@hotmail.com>
2024-06-07 14:19:05 -0500

    Add test unitarios

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from swagger import schema_view
+from doc.swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
